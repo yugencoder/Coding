cd3f2d265a8f5deb5d80e7594ba86cbe3a11738f
sharin nair <sharin.fp@gmail.com>
2020-04-18 16:50:49 +0800

    p#1410 - html parser

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	testCases := []string{}
+
+	for _, tc := range testCases {
+		fmt.Println("Input : %v", tc)
+	}
+}
---
+++ medium/1410.go
@@ -0,0 +1,37 @@
+package p1410
+
+func EntityParser(text string) string {
+	var start int
+	mapping := map[string]rune{
+		"&quot":  '"',
+		"&apos":  '\'',
+		"&amp":   '&',
+		"&gt":    '>',
+		"&lt":    '<',
+		"&frasl": '/',
+	}
+
+	for i := 0; i < len(text); i++ {
+		t := text[i]
+		if t == '&' {
+			start = i
+		}
+
+		if t == ';' && start != -1 {
+			matchString := text[start:i]
+			val, ok := mapping[matchString]
+			if ok {
+				var suffix string
+				if i != len(text) {
+					suffix = text[i+1:]
+				}
+				text = text[:start] + string(val)
+				text += suffix
+				i = start
+				start = -1
+			}
+		}
+	}
+
+	return text
+}
