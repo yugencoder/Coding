69f6c476c8333de6ca341a2ab435cc4c2f9454f1
Sharin Nair <yugencoder@gmail.com>
2021-11-29 17:20:04 +0800

    add aiq questions

--- contest/aiq/Q4.go
+++ contest/aiq/Q4.go
@@ -13,37 +13,59 @@ func distanceMetric(nums []int) []int {
 	lastMap := map[int]prevData{}
 
 	for i, n := range nums {
-		if prev, ok := lastMap[n]; !ok {
-			lastMap[n] = prevData{
-				idx:   i + 1,
-				count: 1,
+		prev, ok := lastMap[n]
+		if !ok {
+			prev = prevData{
+				count: 0,
 			}
-			left[i] = (prev.idx)*prev.count + i + 1 - prev.idx
+
+		} else {
+			left[i] = left[prev.idx] + (i-prev.idx)*prev.count
 		}
 
-		prev := lastMap[n]
-		prev.count += 1
-		prev.idx = i
+		lastMap[n] = prevData{
+			idx:   i,
+			count: prev.count + 1,
+		}
+
 	}
 
 	lastMap = map[int]prevData{}
 	for i := len(nums) - 1; i >= 0; i-- {
 		n := nums[i]
-		if prev, ok := lastMap[n]; !ok {
-			lastMap[n] = prevData{
-				idx:   i + 1,
-				count: 1,
+		prev, ok := lastMap[n]
+		if !ok {
+			prev = prevData{
+				count: 0,
 			}
-			right[i] = (len(nums)-prev.idx)*prev.count + prev.idx + 1 - i
+
+		} else {
+			right[i] = right[prev.idx] + (prev.idx-i)*prev.count
 		}
 
-		prev := lastMap[n]
-		prev.count += 1
-		prev.idx = i
+		lastMap[n] = prevData{
+			idx:   i,
+			count: prev.count + 1,
+		}
 	}
 
 	for i := range nums {
 		res[i] = left[i] + right[i]
+	}
+
+	return res
+}
+
+func distanceMetric2(nums []int) []int {
+	res := make([]int, len(nums))
+
+	for i := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				res[i] += j - i
+				res[j] += j - i
+			}
+		}
 	}
 
 	return res
--- contest/aiq/Q4_test.go
+++ contest/aiq/Q4_test.go
@@ -19,13 +19,13 @@ func Test_distanceMetric(t *testing.T) {
 			args: args{
 				nums: []int{1, 2, 1, 1, 2, 3},
 			},
-			want: nil,
+			want: []int{5, 3, 3, 4, 3, 0},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := distanceMetric(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+			if got := distanceMetric2(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("distanceMetric() = %v, want %v", got, tt.want)
 			}
 		})
